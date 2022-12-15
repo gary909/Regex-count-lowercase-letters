c77f8f48757c24153cee689c432a859bd4fb728a
gary909 <garywhite61@hotmail.com>
2022-12-15 13:11:25 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function lowercaseCount(str) {
+    //How many?
+}
+
+console.log(lowercaseCount("abc")); // 3
+console.log(lowercaseCount("abcABC123")); // 3
+console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~")); // 3
+console.log(lowercaseCount("")); // 0
+console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~")); // 0
+console.log(lowercaseCount("abcdefghijklmnopqrstuvwxyz")); // 26
