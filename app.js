df9d0cba1401166ebaac9817e3f101a0e97a3633
gary909 <garywhite61@hotmail.com>
2022-12-15 13:51:13 +0100

    code working, at last!

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 function lowercaseCount(str) {
-    //How many?
+    //remove all non-alphanumeric chars;
+    let myStr = str.replace(/[^a-z0-9]/gi, '');
+    //remove numbers;
+    myStr = myStr.replace(/[0-9]/g, '');
+    let myStr2 = myStr.toLowerCase()
+    let myCnt = 0;
+
+    if (myStr == '') { // check for empty
+        return myCnt;
+    }
+
+    for (let i = 0; i < myStr.length; i++) {
+        if (myStr[i] === myStr2[i]) { // check for lower
+            myCnt = myCnt + 1;
+        }
+    }
+    return myCnt;
 }
 
 console.log(lowercaseCount("abc")); // 3
